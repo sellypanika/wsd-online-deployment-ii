ad181d2cb9e4c07078966d2d6bb3002a8a71bacb
sellypanika <selly15panika@outlook.com>
2024-10-02 22:26:42 +0300

    deployment-3

--- services/messageService.js
+++ services/messageService.js
@@ -1,6 +1,6 @@
 import { sql } from "../database/database.js";
 
-const recentMessages = async function getRecentMessages(limit = 5) {
+const recentMessages = async function (limit = 5) {
   return await sql`
     SELECT sender, message FROM messages
     ORDER BY id DESC
@@ -8,7 +8,7 @@ const recentMessages = async function getRecentMessages(limit = 5) {
   `;
 };
 
-const addMessage = async function addMessage(sender, message) {
+const addMessage = async function (sender, message) {
   await sql`
     INSERT INTO messages (sender, message)
     VALUES (${sender}, ${message})
